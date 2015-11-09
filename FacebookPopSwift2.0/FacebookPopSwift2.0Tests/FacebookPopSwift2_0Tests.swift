b63c061e1fc7fc285b5142003b8d28bdc47cd203
KBryan <kwame@3einteractive.com>
2015-11-09 10:29:35 -0500

    Initial Commit

---
+++ FacebookPopSwift2.0/FacebookPopSwift2.0Tests/FacebookPopSwift2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FacebookPopSwift2_0Tests.swift
+//  FacebookPopSwift2.0Tests
+//
+//  Created by KBryan on 2015-11-09.
+//  Copyright © 2015 KBryan. All rights reserved.
+//
+
+import XCTest
+@testable import FacebookPopSwift2_0
+
+class FacebookPopSwift2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
